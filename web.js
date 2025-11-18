8a0117d6491a1de07901e9cfaff2744440af7059
rishikaj0205 <rishikaj0205@gmail.com>
2025-11-18 14:28:29 +0530

    folder s added

---
+++ web.js
@@ -0,0 +1,7 @@
+function menu(){
+    document.getElementById('kiwi').style.display='block'
+    document.getElementById('crs').style.display='inline-block'
+}
+function close(){
+    document.getElementById('kiwi').style.display='none'
+}
